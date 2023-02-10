6682f5fa9e816e75052c0609a1b462490ddedcff
Mirzam2 <666mopsegor@gmail.com>
2023-02-10 17:30:27 +0300

    TOR

--- lab1/tor.cpp
+++ lab1/tor.cpp
@@ -5,15 +5,14 @@ int make_tor(float r, float R)
 {
     int nc = 3; // number of centers
     int np = 9; // number of points in one mini circle
-    double lc = 1e-1;
+    double lc = 1e-2;
     int centers[nc];
-    int circles[nc];
     int points[nc][np];
+    int arcs[nc][np];
     std::vector<int> surf;
-    surf.assign(nc, 0);
+    surf.assign(nc * np, 0);
     for (int i = 0; i < nc; ++i)
     {
-        int arcs[np];
         float a = 2 * i * M_PI / nc;
         centers[i] = gmsh::model::geo::addPoint(R * cos(a), R * sin(a), 0, lc);
 
@@ -25,25 +24,15 @@ int make_tor(float r, float R)
 
         for (int j = 0; j < np - 1; ++j)
         {
-            arcs[j] = gmsh::model::geo::addCircleArc(points[i][j], centers[i], points[i][j + 1]);
+            arcs[i][j] = gmsh::model::geo::addCircleArc(points[i][j], centers[i], points[i][j + 1]);
         }
-        arcs[np - 1] = gmsh::model::geo::addCircleArc(points[i][np - 1], centers[i], points[i][0]);
-        std::vector<int> vec;
+        arcs[i][np - 1] = gmsh::model::geo::addCircleArc(points[i][np - 1], centers[i], points[i][0]);
         for (int j = 0; j < np; ++j)
         {
-            vec.push_back(arcs[j]);
-            std::cout << vec[j] << '\n';
+            std::vector<std::pair<int, int>> ov;
+            gmsh::model::geo::revolve({{1, arcs[i][j]}}, 0, 0, 0, 0, 0, 1, 2 * M_PI / nc, ov);
+            surf[i * np + j] = ov[1].second;
         }
-        circles[i] = gmsh::model::geo::addCurveLoop({vec});
-        std::vector<std::pair<int, int>> ov;
-        gmsh::model::geo::revolve({{1, circles[i]}}, 0, 0, 0, 0, 0, 1, 2 * M_PI / nc, ov);
-        surf[i] = ov[0].second;
-        std::cout << "i " << i << "surf: " << surf[i] << '\n';
-    }
-    std::cout << "Cicle work\n";
-    for (int i = 0; i < nc; ++i)
-    {
-        std::cout << circles[i] << " ";
     }
     return gmsh::model::geo::addSurfaceLoop(surf);
 }
@@ -53,11 +42,11 @@ int main(int argc, char **argv)
 
     gmsh::model::add("tor");
 
-    float r0 = 0.3, r1 = 0.2, r2 = 0.1;
-    int t1 = make_tor(r1, r0);
-    // int t2 = make_tor(r2, r0);
+    float r0 = 0.4, r1 = 0.2, r2 = 0.1;
+    auto t1 = make_tor(r1, r0);
+    auto t2 = make_tor(r2, r0);
 
-    // gmsh::model::geo::addVolume({t1, -t2});
+    gmsh::model::geo::addVolume({t1,-t2});
     gmsh::model::geo::synchronize();
 
     gmsh::model::mesh::generate(3);
