bf990e3dc588fd46d875eeb176d59d5a8680c236
Mirzam2 <666mopsegor@gmail.com>
2023-04-05 23:42:45 +0300

    Добавил гравитацию, пока кривую, не учитывающую случай когда они близкие

--- MicroProject/main.cpp
+++ MicroProject/main.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <cmath>
 #include <SFML/Graphics.hpp>
+float G = 10;
 class Particle
 {
 private:
-    int mass;
+    int mass = 1;
     float vx = 0;
     float vy = 0;
     float ax = 0;
@@ -27,16 +28,26 @@ private:
         // проверка на слипание
     }
 
-    void grav(Particle *other)
+    float range(Particle &other)
     {
-        // реализует сразу гравитацию и у себя и у того что передали
-    }
-    float range2(Particle &other)
-    {
-        return pow((x - other.x), 2) + pow((y - other.y), 2);
+        return pow(pow((x - other.x), 2) + pow((y - other.y), 2), 0.5);
     }
 
 public:
+    Particle() {}
+    Particle(float x, float y) : x(x), y(y)
+    {
+        image.setPosition(x, y);
+    }
+    void grav(Particle *other)
+    {
+        float f = G * mass * other->mass / pow(range(*other), 3);
+        ax += f * (other->x - x) / mass;
+        ay += f * (other->y - y) / mass;
+        other->ax -= f * (other->x - x) / other->mass;
+        other->ay -= f * (other->y - y) / other->mass;
+        // реализует сразу гравитацию и у себя и у того что передали
+    }
     void move(float dt)
     {
         // использовать библиотеку https://numerary.readthedocs.io/ru/latest/# что бы использовать метод рангекуты 4 точности Dormand–Prince method
@@ -44,7 +55,7 @@ public:
         vy += ay * dt;
         x += vx * dt;
         y += vy * dt;
-        image.setPosition(x, y); // можно сделать подгонку под начальные параметры окна
+        image.setPosition(x, y); // можно сделать подгонку под начальные параметры окна, делать не каждый шаг
     }
     void draw(sf::RenderWindow *window)
     {
@@ -70,10 +81,11 @@ int main()
     // sf::VertexArray massiv(sf::Triangles, n);
     for (int i = 0; i < n; ++i)
     {
-        arr[i].givev(1 * i, i * i);
+        arr[i] = Particle(50 * i, 20);
+        //arr[i].givev(1 * i, i * i);
     }
     sf::CircleShape shape(10.f);
-    int a = 0;
+    int FPS = 10;
     while (window.isOpen())
     {
         sf::Event event;
@@ -82,16 +94,22 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                arr[i].grav(&arr[j]);
+            }
+        }
         window.clear();
         for (int i = 0; i < n; ++i)
         {
             // std::cout << i << ' ';
             // arr[i].print_data();
             arr[i].move(1);
-            arr[i].draw(&window);
+            arr[i].draw(&window); // отрисовку можно делать не каждый шаг, так же можно разделить вычисления и отрисовку, например сохранять в текстовик координаты тел
         }
-        a++;
+        sf::sleep(sf::seconds(1.0 / FPS)); // надо сделать переменным, что бы не тормозить когда и так не надо
         window.display();
     }
     delete[] arr;
